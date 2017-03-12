d41ad4daa455b7162ac5a8a10c18ce7026c82637
Kerry McConnaughay <kmmcconnaughay@gmail.com>
2017-03-11 23:44:33 -0500

    Turning in word frequency toolbox

--- frequency.py
+++ frequency.py
@@ -10,10 +10,23 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    # This open the file and makes the text lowercase
+    text = open(file_name, 'r')
+    # splits the lines into words
+    split_words = []
+    for string in text:
+        split = string.split()
+        split_words.extend(split)
+    # strips the words of punctuation and whitespace
+    stripped_words = []
+    for word in split_words:
+        word = word.lower()
+        word = word.strip("/.,?!':;[]\")(%\r\n")
+        stripped_words.append(word)
+    return stripped_words
 
 
-def get_top_n_words(word_list, n):
+def get_top_n_words(stripped_words, n):
     """ Takes a list of words as input and returns a list of the n most frequently
     occurring words ordered from most to least frequently occurring.
 
@@ -21,10 +34,21 @@ def get_top_n_words(word_list, n):
     punctuation
     n: the number of words to return
     returns: a list of n most frequently occurring words ordered from most
-    frequently to least frequentlyoccurring
+    frequently to least frequently occurring.
     """
-    pass
+    d = {}
+    for word in stripped_words:
+        d[word] = d.get(word, 0) + 1
+    ordered_words = sorted(d, key=d.get, reverse=True)
+    top_frequencies = ordered_words[:n]
+    for word in top_frequencies:
+        punctuation = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+        for ch in word:
+            if ch in punctuation: top_frequencies.remove(word)
+    print(top_frequencies)
+    return top_frequencies
 
 if __name__ == "__main__":
-    print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    print('Running WordFrequency Toolbox')
+    stripped_words = get_word_list('wizard.txt')
+    get_top_n_words(stripped_words, 100)
